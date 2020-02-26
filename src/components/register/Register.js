ba2706302a80e5d46475c40c8d17ab88906615ac
Raphael Imfeld <raphael.imfeld@uzh.ch>
2020-02-26 14:49:00 +0100

    New layout with icons

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -36,12 +36,6 @@ const Form = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 `;
 
-const Label = styled.label`
-  color: white;
-  margin-bottom: 10px;
-  text-transform: uppercase;
-`;
-
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: center;
