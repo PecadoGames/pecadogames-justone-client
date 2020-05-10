7ec71c9741d233a1541c5e5098fd008166a01691
rmogicato <55449984+rmogicato@users.noreply.github.com>
2020-05-10 17:24:19 +0200

    changed display of time FS20G04-128
    bigger & color changes when time is running out

--- src/components/game/GameInfos/Timer.js
+++ src/components/game/GameInfos/Timer.js
@@ -1,8 +1,23 @@
 import {api, handleError} from "../../../helpers/api";
-
+import styled from "styled-components";
 import React from "react";
 import {withRouter} from "react-router-dom";
 
+const Box = styled.div`
+    border: 2px solid white;
+    width: 110px;
+    height: auto;
+    text-align: center;
+`;
+
+const NormalTime = styled.text`
+    font-size: 40px;
+    color: white;
+`;
+
+const TimerLow = styled(NormalTime)`
+    color: #ad151c;
+`;
 
 class Timer extends React.Component{
     constructor() {
@@ -33,9 +48,19 @@ class Timer extends React.Component{
         clearInterval(this.state.interval)
     }
 
+    displayTime(){
+        if (this.state.timer < 10){
+            return <TimerLow>00:0{this.state.timer}</TimerLow>
+        }
+        else{
+            return <NormalTime>00:{this.state.timer}</NormalTime>
+        }
+    }
 
     render(){
-        return( <div>Timer: {this.state.timer}</div> )
+        return(
+            <Box>{this.displayTime()}</Box>
+        )
     }
 }
 export default withRouter(Timer);
